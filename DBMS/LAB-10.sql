9fdf218199f67b58cd14dd0b0b093e184423aa38
jay baraiya <140989637+jaybaraiya1@users.noreply.github.com>
2024-09-11 10:23:47 +0530

    Update LAB-10.sql

--- DBMS/LAB-10.sql
+++ DBMS/LAB-10.sql
@@ -40,8 +40,50 @@ SELECT * FROM Personal
  CREATE VIEW Student_Pattern
  AS
  SELECT RNo,Name, Branch FROM STUDENT_INFO WHERE Name LIKE '____'
- SELECT * FROM Student_Pattern --6. Insert a new record to AcademicData view. (107, Meet, ME)INSERT INTO AcademicData VALUES(107, 'Meet', 'ME') SELECT * FROM AcademicData --7 Update the branch of Amit from CE to ME in Student_Details view UPDATE Student_Details SET Branch='ME' WHERE NAME='AMIT'  SELECT * FROM Student_Details  --8. Delete a student whose roll number is 104 from AcademicData view.DELETE FROM AcademicData WHERE RNo=104--PART B----1. Create a view that displays information of all students whose SPI is above 8.5   CREATE VIEW students   AS   SELECT SPI FROM STUDENT_INFO WHERE SPI>8.5--2. Create a view that displays 0 backlog students.   CREATE VIEW students   AS   SELECT Bklog FROM STUDENT_INFO WHERE Bklog=0--3. Create a view Computerview that displays CE branch data only. CREATE VIEW Computerview   AS   SELECT branch FROM STUDENT_INFO WHERE branch='CE'   SELECT * FROM Computerview--PART C----1. Create a view Result_EC that displays the name and SPI of students with SPI less than 5 of branch EC.   CREATE VIEW Result_EC   AS   SELECT SPI,NAME  FROM STUDENT_INFO WHERE SPI<5 AND branch='EC'   SELECT * FROM Result_EC--2. Update the result of student MAHESH to 4.90 in Result_EC view.    UPDATE Result_EC SET SPI=4.90 WHERE NAME='Mahesh'--3. Create a view Stu_Bklog with RNo, Name and Bklog columns in which name starts with ‘M’ and having bklogs more than 5.
-   CREATE VIEW Stu_Bklog   AS   SELECT RNo,NAME,Bklog  FROM STUDENT_INFO WHERE NAME LIKE 'M%' AND Bklog>5
+ SELECT * FROM Student_Pattern
+
+ --6. Insert a new record to AcademicData view. (107, Meet, ME)
+INSERT INTO AcademicData VALUES(107, 'Meet', 'ME')
+ SELECT * FROM AcademicData
+
+ --7 Update the branch of Amit from CE to ME in Student_Details view
+ UPDATE Student_Details SET Branch='ME' WHERE NAME='AMIT'
+  SELECT * FROM Student_Details
+
+  --8. Delete a student whose roll number is 104 from AcademicData view.
+DELETE FROM AcademicData WHERE RNo=104
+
+--PART B--
+
+--1. Create a view that displays information of all students whose SPI is above 8.5
+   CREATE VIEW students
+   AS
+   SELECT SPI FROM STUDENT_INFO WHERE SPI>8.5
+--2. Create a view that displays 0 backlog students.
+   CREATE VIEW students
+   AS
+   SELECT Bklog FROM STUDENT_INFO WHERE Bklog=0
+
+--3. Create a view Computerview that displays CE branch data only.
+ CREATE VIEW Computerview
+   AS
+   SELECT branch FROM STUDENT_INFO WHERE branch='CE'
+   SELECT * FROM Computerview
+--PART C--
+
+--1. Create a view Result_EC that displays the name and SPI of students with SPI less than 5 of branch EC.
+   CREATE VIEW Result_EC
+   AS
+   SELECT SPI,NAME  FROM STUDENT_INFO WHERE SPI<5 AND branch='EC'
+   SELECT * FROM Result_EC
+--2. Update the result of student MAHESH to 4.90 in Result_EC view.
+    UPDATE Result_EC SET SPI=4.90 WHERE NAME='Mahesh'
+
+
+--3. Create a view Stu_Bklog with RNo, Name and Bklog columns in which name starts with â€˜Mâ€™ and having bklogs more than 5.
+   CREATE VIEW Stu_Bklog
+   AS
+   SELECT RNo,NAME,Bklog  FROM STUDENT_INFO WHERE NAME LIKE 'M%' AND Bklog>5
      SELECT * FROM Stu_Bklog
 
 --4. Drop Computerview form the database.
